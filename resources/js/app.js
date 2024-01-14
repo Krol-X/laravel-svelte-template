1c28256fc91914f8b86c960fc59315952559ef3d
Alex Krol <krolmail@list.ru>
2024-01-14 20:19:23 +0300

    Inital

---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+import './bootstrap';
+import {createInertiaApp} from '@inertiajs/svelte'
+
+import Layout from './Layouts/Layout.svelte'
+
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.svelte', {eager: true})
+    const page = pages[`./Pages/${name}.svelte`]
+    return {default: page.default, layout: Layout}
+  },
+  setup({el, App, props}) {
+    new App({target: el, props})
+  },
+})
